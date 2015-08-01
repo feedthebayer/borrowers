0f401bbced00c575cc2e9dd17731700e96d1fec0
Brandon Bayer <feedthebayer@users.noreply.github.com>
2015-08-01 17:55:42 -0500

    feat: delete friend

--- app/controllers/friends/new.js
+++ app/controllers/friends/new.js
@@ -3,6 +3,7 @@ import FriendsBaseController from './base';
 export default FriendsBaseController.extend({
   actions : {
     cancel() {
+      this.get('model').destroyRecord();
       this.transitionToRoute('friends.index');
       return false;
     }
